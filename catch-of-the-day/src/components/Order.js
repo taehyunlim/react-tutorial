2a9d0f9ca676b7ef242872971b26c48d6b08c909
Taehyun Lim <taehyun.lim@gmail.com>
2018-03-15 00:38:55 -0700

    Added more components

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Order extends React.Component {
+    render() {
+        return (
+            <div className="Order">
+                Test
+            </div>           
+        )
+    }
+}
+
+export default Order;
